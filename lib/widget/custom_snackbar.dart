22415eb8b9a0c992705d36a13e53d546e56f7a20
amey2027 <amey.vagare03.gmail.com>
2026-05-26 13:04:07 +0530

    all done

--- lib/widget/custom_snackbar.dart
+++ lib/widget/custom_snackbar.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class CustomSnackbar{
   
   showCustomSnackbar(BuildContext context , { required String? message, required Color? bgColor}){
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message!), backgroundColor: bgColor));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message!, style: TextStyle(fontSize: 18)), backgroundColor: bgColor));
   }
 
 }
